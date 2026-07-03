1340efa3f538468e8509de80e692ee1aa3f6b2a1
nokakoding <barrenofrancisakon@gmail.com>
2026-07-03 10:00:32 +0800

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -6,7 +6,6 @@ full_name = 'Francis Akon Barreno'
 country = 'Philippines'
 city = 'Cagayan de Oro City'
 age = 18
-year = '2026'
 is_married = False
 skills = ['communication', 'empathy', 'adaptability']
 person_info = {
