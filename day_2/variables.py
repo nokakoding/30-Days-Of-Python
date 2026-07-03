7289ee9f607d1a5477e2f195eac4165477e8a4e7
nokakoding <barrenofrancisakon@gmail.com>
2026-07-03 09:39:06 +0800

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,1 +1,10 @@
-# Day 2
+# Day 2: 30 Days of python programming
+
+first_name = 'Francis Akon'
+last_name = 'Barreno'
+full_name = 'Francis Akon Barreno'
+country = 'Philippines'
+city = 'Cagayan de Oro City'
+age = '18'
+year = '2026'
+is_married = False
