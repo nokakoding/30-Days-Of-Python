101f133b54f5e0cdc7ec980f43092017e494e692
nokakoding <barrenofrancisakon@gmail.com>
2026-07-03 10:23:54 +0800

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -26,3 +26,8 @@ print('Age: ', age)
 print('Married: ', is_married)
 print('Skills: ', skills)
 print('Person information: ', person_info)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, is_married = 'Francis Akon', 'Barreno', 'Philippines', 18, False
+ 
