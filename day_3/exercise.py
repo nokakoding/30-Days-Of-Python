34d2e2f62b0355335e33e3a1e4b40c2437764a3a
nokakoding <barrenofrancisakon@gmail.com>
2026-07-06 18:10:44 +0800

    Update exercise.py

--- day_3/exercise.py
+++ day_3/exercise.py
@@ -2,6 +2,8 @@ age = int(18)
 height = float(5.5)
 weight = complex(60kg)
 
+area = 0.5 * Base * Height
+
 print(input('Base: '))
 print(input('Height: '))
-
+print('The area of the triangle is' + area)
